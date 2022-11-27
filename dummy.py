e9b0a6fdaece8401de903e6722f4394d6df3517b
varsha <varshhhhax.s@gmail.com>
2022-11-27 10:14:55 +0530

    Create dummy.py

---
+++ dummy.py
@@ -0,0 +1,5 @@
+#area of a room
+Lenght=float(input("Enter the lenght of the room:"))
+Width=float(input("Enter the width of the room:")) 
+area=Lenght*Width
+print("Area of the room is",area,"sq.fts")
